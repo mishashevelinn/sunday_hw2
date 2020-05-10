1cc53968c2349370556784947dcc6feffecb5f50
misha <shevel55505@gmail.com>
2020-05-10 23:18:43 +0300

    pushing code

---
+++ cmake-build-debug/CMakeFiles/sunday_hw.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/misha/CLionProjects/sunday-hw/League.c" "/home/misha/CLionProjects/sunday-hw/cmake-build-debug/CMakeFiles/sunday_hw.dir/League.c.o"
+  "/home/misha/CLionProjects/sunday-hw/Match.c" "/home/misha/CLionProjects/sunday-hw/cmake-build-debug/CMakeFiles/sunday_hw.dir/Match.c.o"
+  "/home/misha/CLionProjects/sunday-hw/Team.c" "/home/misha/CLionProjects/sunday-hw/cmake-build-debug/CMakeFiles/sunday_hw.dir/Team.c.o"
+  "/home/misha/CLionProjects/sunday-hw/main.c" "/home/misha/CLionProjects/sunday-hw/cmake-build-debug/CMakeFiles/sunday_hw.dir/main.c.o"
+  "/home/misha/CLionProjects/sunday-hw/sort-league.c" "/home/misha/CLionProjects/sunday-hw/cmake-build-debug/CMakeFiles/sunday_hw.dir/sort-league.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/sunday_hw.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sunday_hw.dir/League.c.o"
+  "CMakeFiles/sunday_hw.dir/Match.c.o"
+  "CMakeFiles/sunday_hw.dir/Team.c.o"
+  "CMakeFiles/sunday_hw.dir/main.c.o"
+  "CMakeFiles/sunday_hw.dir/sort-league.c.o"
+  "sunday_hw"
+  "sunday_hw.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sunday_hw.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
